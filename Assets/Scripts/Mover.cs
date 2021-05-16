b76747c56ff48cbbf4e5d31597a6f9f4bd8c3939
mustafamese <mustafamese099@gmail.com>
2021-05-16 23:46:56 +0300

    -Knife fall mechanic added.

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -5,56 +5,62 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
+    private enum Phase { RISE, FALL }
+    private Phase phase;
+
     [SerializeField] Rigidbody rb;
     [SerializeField] float distance;
     [SerializeField] float smooth;
-    [SerializeField] Vector3 targetVector = new Vector3(1, 0, 0);
+
+    [SerializeField] Vector3 riseVector;
+    [SerializeField] Vector3 fallVector;
+
+    private Vector3 targetVector;
 
     private Vector3 targetPos;
     private bool canMove;
 
     private void FixedUpdate()
     {
-        if(canMove)
+        if (canMove)
             Move();
     }
+
 
     private void Move()
     {
         transform.position = Vector3.MoveTowards(transform.position, targetPos, Time.deltaTime * smooth);
         if (Vector3.Distance(transform.position, targetPos) < distance)
         {
-            canMove = false;
-            ConfigureRigidbody(false);
-            targetPos = Vector3.zero;
+            phase = Phase.FALL;
+            SetTarget();
         }
+    }
+
+    private void SetTarget()
+    {
+        switch (phase)
+        {
+            case Phase.RISE:
+                targetVector = riseVector;
+                break;
+            case Phase.FALL:
+                targetVector = fallVector;
+                break;
+        }
+
+        targetPos = transform.position + targetVector;
     }
 
     public void StopOrder()
     {
         canMove = false;
-        ConfigureRigidbody(true);
     }
 
     public void FlipOrder()
     {
-        targetPos = transform.position + targetVector;
         canMove = true;
-        ConfigureRigidbody(true);
-    }
-
-    private void ConfigureRigidbody(bool close)
-    {
-        if(close)
-        {
-            rb.isKinematic = true;
-            rb.useGravity = false;
-        }
-        else
-        {
-            rb.useGravity = true;
-            rb.isKinematic = false;
-        }
-        
+        phase = Phase.RISE;
+        SetTarget();
     }
 }
