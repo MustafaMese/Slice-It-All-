b3a221f0b2192f368a74b6e7c2c12a148fe72734
mustafamese <mustafamese099@gmail.com>
2021-05-17 21:49:48 +0300

    -Cut added.

--- Assets/Scripts/TriggerReceiver.cs
+++ Assets/Scripts/TriggerReceiver.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using EzySlice;
 using UnityEngine;
 
 public class TriggerReceiver : MonoBehaviour
@@ -9,6 +11,41 @@ public class TriggerReceiver : MonoBehaviour
 
     private void OnTriggerEnter(Collider other) 
     {
-        trigger.Receive(triggerType);
+        switch (triggerType)
+        {
+            case TriggerType.EDGE:
+                if (other.tag == "Platform")
+                    trigger.Receive(triggerType);
+                else if (other.tag == "Cuttable")
+                    Cut(other);
+                break;
+            case TriggerType.GRASP:
+                trigger.Receive(triggerType);
+                break;
+        }
+        
+    }
+
+    private void Cut(Collider other)
+    {
+        GameObject shorn = other.gameObject;
+        SlicedHull hull = shorn.Slice(transform.position, transform.forward);
+    
+        if(hull != null)
+        {
+            GameObject lower = hull.CreateLowerHull(shorn, shorn.GetComponent<Renderer>().material);
+            GameObject upper = hull.CreateUpperHull(shorn, shorn.GetComponent<Renderer>().material);
+
+            Rigidbody rb1 = lower.AddComponent<Rigidbody>();
+            lower.AddComponent<MeshCollider>().convex = true;
+            rb1.AddForce(-transform.forward * 200f, ForceMode.Force);
+
+            Rigidbody rb2 = upper.AddComponent<Rigidbody>();
+            upper.AddComponent<MeshCollider>().convex = true;
+            rb2.AddForce(transform.forward * 200f, ForceMode.Force);
+
+        }
+
+        shorn.SetActive(false);
     }
 }
